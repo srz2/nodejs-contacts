9b316026ce7d38b3fb39e6fcadf9680ce26265ef
srz2 <srz222@gmail.com>
2020-03-13 22:15:49 -0400

    Moved api env and npm package jsons to root

--- test/contacts.js
+++ test/contacts.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe("Contact Logic", function() {
-    const Contact = require('../modules/contact');
+    const Contact = require('../api/src/modules/contact');
 
     describe("Creation Logic", function() {
         it("Create New Contact - Not Nulled", function () {
