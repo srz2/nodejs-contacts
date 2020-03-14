e8bbf7f769ca14a767c7fc06b7653e21e272f35e
srz2 <srz222@gmail.com>
2020-03-14 17:09:05 -0400

    Dynamically got host IP Address

--- api/src/config.js
+++ api/src/config.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 
+// Get IP Address
+var addr;
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('addr: ' + add);
+})
+
 module.exports = {
     PORT: process.env.PORT,
-    IP_ADDRESS: process.env.IP_ADDRESS,
+    IP_ADDRESS: addr,
     MONGO: {
         USER: process.env.MONGO_USER,
         PASSWORD: process.env.MONGO_PASSWORD,
